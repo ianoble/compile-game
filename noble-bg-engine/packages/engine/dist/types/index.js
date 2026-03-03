d517716b15d42120c5a510b9f9db5349e76b319f
Ian Noble <ianoble@gmail.com>
2026-03-03 10:26:54 -0800

    Remove node_modules and dist from Git tracking
    - Remove node_modules directories from Git tracking
    - Remove dist build output from Git tracking
    - These directories are properly ignored by .gitignore but were tracked before being added to ignore

--- noble-bg-engine/packages/engine/dist/types/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redactCards = exports.isCardHidden = exports.defineGame = void 0;
-var framework_js_1 = require("./framework.js");
-Object.defineProperty(exports, "defineGame", { enumerable: true, get: function () { return framework_js_1.defineGame; } });
-var cards_js_1 = require("./cards.js");
-Object.defineProperty(exports, "isCardHidden", { enumerable: true, get: function () { return cards_js_1.isCardHidden; } });
-Object.defineProperty(exports, "redactCards", { enumerable: true, get: function () { return cards_js_1.redactCards; } });
